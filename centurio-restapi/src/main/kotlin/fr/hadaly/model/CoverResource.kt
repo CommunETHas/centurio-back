b5afdf95fcdc9700504dff44324f2b9edfbb336d
Bogdan <ooctogene@gmail.com>
2021-06-22 12:41:15 +0200

    feat: full cover initialization

--- centurio-restapi/src/main/kotlin/fr/hadaly/model/CoverResource.kt
+++ centurio-restapi/src/main/kotlin/fr/hadaly/model/CoverResource.kt
@@ -2,7 +2,6 @@ package fr.hadaly.model
 
 import fr.hadaly.nexusapi.model.Chain
 import fr.hadaly.nexusapi.model.CoverType
-import fr.hadaly.nexusapi.model.serializer.DateAddedSerializer
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -11,7 +10,6 @@ data class Cover(
     val address: String,
     val type: CoverType,
     val supportedChains: List<Chain> = emptyList(),
-    @Serializable(with = DateAddedSerializer::class)
     val logo: String? = null,
     val coveredToken: String? = null
 )
