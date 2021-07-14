5d547541bb57b85f67d65474cd6a4922ea14f224
Bogdan <ooctogene@gmail.com>
2021-07-14 10:12:36 +0200

    build: improve version management

--- nexus-api/build.gradle.kts
+++ nexus-api/build.gradle.kts
@@ -1,12 +1,10 @@
-val ktorVersion = "1.6.1"
-
 plugins {
-    kotlin("plugin.serialization") version "1.5.20"
+    kotlin("plugin.serialization") version "1.5.21"
 }
 
 dependencies {
-    implementation("org.web3j:core:5.0.0")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-json:$ktorVersion")
-    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("org.web3j:core:${rootProject.extra["web3j"]}")
+    implementation("io.ktor:ktor-client-cio:${rootProject.extra["ktorVersion"]}")
+    implementation("io.ktor:ktor-client-json:${rootProject.extra["ktorVersion"]}")
+    implementation("io.ktor:ktor-client-serialization:${rootProject.extra["ktorVersion"]}")
 }
